f8c6618fe66b4cdff38a8e93933bb5eeea028022
Patel Chirag <chiragp2013@gmail.com>
2023-11-04 11:38:49 +0000

    first commit Here

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,24 @@
 // Write your code below
+// FizzBuzz logic
+for (let i = 1; i <= 100; i++) {
+    let output = '';
+
+    if (i % 3 === 0) {
+        output += 'Fizz';
+    }
+
+    if (i % 5 === 0) {
+        output += 'Buzz';
+    }
+
+    if (output === '') {
+        output = i;
+    }
+
+    // Create a list item for each FizzBuzz number
+    const listItem = document.createElement('li');
+    listItem.textContent = output;
+
+    // Add the list item to the output list
+    document.getElementById('output-list').appendChild(listItem);
+}
